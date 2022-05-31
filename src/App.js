f82e2f55de05e1b971a29ff1f4430379420ee707
SEANHXE <790438599@qq.com>
2022-05-30 23:26:05 -0400

    add function of Update Employee

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import React from "react"
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom'
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 import HeaderComponent from "./components/HeaderComponent"
 import FooterComponent from "./components/FooterComponent"
 import CreateEmployeeComponent from "./components/CreateEmployeeComponent"
+import UpdateEmployeeComponent from "./components/UpdateEmployeeComponent"
 
 
 function App() {
   return (
       <div>
-          <Router>
+          <Router forceRefresh={true}>
               <HeaderComponent/>
               <div className="container">
                   <Switch>
-                      <Route path = "/" exact component = {ListEmployeeComponent}></Route>
-                      <Route path = "/employees" component = {ListEmployeeComponent}></Route>
-                      <Route path = "/add-employee" component = {CreateEmployeeComponent}></Route>
+                      <Route path = "/employees" component = {ListEmployeeComponent}/>
+                      <Route path = "/add-employee" component = {CreateEmployeeComponent}/>
+                      <Route path = "/update-employee/:id" component = {UpdateEmployeeComponent}/>
+                      <Route exact path = "/" component = {ListEmployeeComponent}/>
+                      <Redirect to = "/" component={ListEmployeeComponent}/>
                   </Switch>
               </div>
               <FooterComponent/>
--- src/components/CreateEmployeeComponent.jsx
+++ src/components/CreateEmployeeComponent.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import EmployeeService from "../services/EmployeeService"
 
 class CreateEmployeeComponent extends Component {
     constructor(props){
@@ -11,7 +12,7 @@ class CreateEmployeeComponent extends Component {
         }
         this.changeLastNameHandler = this.changeLastNameHandler.bind(this);
         this.changeFirstNameHandler = this.changeFirstNameHandler.bind(this);
-        this.saveEmployee = this.saveEmployee.bind(this);;
+        this.saveEmployee = this.saveEmployee.bind(this);
     }
 
     changeFirstNameHandler = (event) => {
@@ -30,6 +31,11 @@ class CreateEmployeeComponent extends Component {
         e.preventDefault();
         let employee = {firstName: this.state.firstName, lastName: this.state.lastName, emailId: this.state.emailId};
         console.log('employee =>' + JSON.stringify(employee));
+
+        EmployeeService.createEmployee((employee)).then(res => {
+            this.props.history.push('employees');
+        });
+
     }
 
     cancel() {
