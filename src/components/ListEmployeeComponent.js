bb74090663828bd774812978a27b00645ad6667e
SEANHXE <790438599@qq.com>
2022-05-31 03:50:47 -0400

    add function of Delete Employee

--- src/components/ListEmployeeComponent.js
+++ src/components/ListEmployeeComponent.js
@@ -10,9 +10,18 @@ class ListEmployeeComponent extends Component {
         }
         this.addEmployee = this.addEmployee.bind(this);
         this.editEmployee = this.editEmployee.bind(this);
+        this.deleteEmployee = this.deleteEmployee.bind(this);
     }
 
     editEmployee(id) {
+        this.props.history.push(`/add-employee/${id}`);
+    }
+
+    deleteEmployee(id) {
+        // rest API
+        EmployeeService.deleteEmployee(id).then(res => {
+            this.setState({employees: this.state.employees.filter(employee => employee.id !== id)});
+        });
         this.props.history.push(`/add-employee/${id}`);
     }
 
@@ -54,6 +63,7 @@ class ListEmployeeComponent extends Component {
                                             <td> {employee.emailId }</td>
                                             <td>
                                                 <button onClick = {() => this.editEmployee(employee.id)} className = "btn, btn-info"> Update </button>
+                                                <button style = {{marginLeft: "10px"}} onClick = {() => this.deleteEmployee(employee.id)} className = "btn, btn-danger"> Delete </button>
                                              </td>
                                         </tr>
                                 )
